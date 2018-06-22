0e8025c590f64721e6fd6aea3efd4e35a3613966
Douglas Estivales <douglas.estivales@iphytus.com>
2018-06-22 16:24:10 -0300

    secao 3 aula 20

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Olá reactawdaw</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>Olá react</h1>, document.getElementById('app'))

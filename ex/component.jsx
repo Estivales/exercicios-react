4083dd9cb9b8a933919789282a7277642b642f64
Douglas Estivales <douglas.estivales@iphytus.com>
2018-06-22 17:19:46 -0300

    secao 3 aula 24

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export default (props) => (
-	<h1>{props.value}</h1>
+export default props => (
+	<h1> Primeiro component! </h1>
 )
 
+const Segundo = props => (
+	<h1>Segundo component! </h1>
+)
+
+export {Segundo}
